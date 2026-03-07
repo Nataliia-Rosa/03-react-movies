0e25914e419dc4d2ffb4437a76149708769c8289
Natalia <rosa.nataliya5@gmail.com>
2026-03-08 00:24:46 +0100

    remove-interface-MoviesResponse

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
-import type { Movie, MoviesResponse } from "../types/movie";
+import type { Movie } from "../types/movie";
 
 const BASE_URL = "https://api.themoviedb.org/3/search/movie";
 
 const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
+interface MoviesResponse {
+  results: Movie[];
+}
 
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
   const response = await axios.get<MoviesResponse>(BASE_URL, {
