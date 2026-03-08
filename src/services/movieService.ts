ab1ee22b9c823aa29d75911b17be9231be7e2d12
Natalia <rosa.nataliya5@gmail.com>
2026-03-08 18:07:02 +0100

    changedToken

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -4,7 +4,7 @@ import type { Movie } from "../types/movie";
 const BASE_URL = "https://api.themoviedb.org/3/search/movie";
 
 const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
-
+console.log("TOKEN:", TOKEN);
 interface MoviesResponse {
   results: Movie[];
 }
@@ -18,5 +18,6 @@ export const fetchMovies = async (query: string): Promise<Movie[]> => {
       Authorization: `Bearer ${TOKEN}`,
     },
   });
+  console.log(response.data.results);
   return response.data.results;
 };
